0ad9005b15f5af4a934f892ae4d299495e0a3e3a
Alejandro Garcia <iacobus75@gmail.com>
2023-01-01 21:50:50 +0100

    Actualizado con .gitignore

---
+++ macFiles/macFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  macFilesApp.swift
+//  macFiles
+//
+//  Created by Alejandro Garcia on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct macFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
